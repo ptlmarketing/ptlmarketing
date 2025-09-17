a4dba0bbeb01b1fa307b771a900a7d57866adb06
Amit914011 <amitmrj914011@gmail.com>
2025-09-17 11:45:36 +0530

    Fixed Error

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
-  // baseURL: "https://ptlmarketing-backend.onrender.com/api/v1",
+  // baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://ptlmarketing-backend.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
