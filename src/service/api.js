8314f50533e3762efef183824ce0cd0944d69635
ptlmarketing <Ptlmarketingg@gmail.com>
2025-10-08 19:13:52 +0530

    Change

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:8080/api/v1",
-  baseURL: "https://ptlmarketing-backend.onrender.com/api/v1",
+  baseURL: "https://ptlmarketing-backend-7fg9.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
